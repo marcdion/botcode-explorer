76c421c901b2b0e7a60c51effc543ae665e3ad48
Marc-Antoine Dion <madion@coveo.com>
2021-12-19 21:43:59 -0500

    chore(api): edited tests for windows

--- api/__tests__/directories.spec.js
+++ api/__tests__/directories.spec.js
@@ -1,5 +1,8 @@
+const os        = require('os');
 const watcher   = require('../dist/logic/watcher').default;
 const store     = require('../dist/data/store').default;
+
+const isWindows = os.platform().includes('win');
 
 describe('watcher - directories', () => {
     it('initDirectories - validate directories', () => {    
@@ -15,38 +18,38 @@ describe('watcher - directories', () => {
         expect(tree.children.length).toBe(2);
 
         //dir
-        expect(tree.children[0].path).toBe('__tests__/testdata/dir');
+        expect(tree.children[0].path).toBe(isWindows ? '__tests__\\testdata\\dir' : '__tests__/testdata/dir');
         expect(tree.children[0].name).toBe('dir');
         expect(tree.children[0].type).toBe('directory');
         expect(tree.children[0].children.length).toBe(3);
 
-        expect(tree.children[0].children[0].path).toBe('__tests__/testdata/dir/bar.js');
+        expect(tree.children[0].children[0].path).toBe(isWindows ? '__tests__\\testdata\\dir\\bar.js' : '__tests__/testdata/dir/bar.js');
         expect(tree.children[0].children[0].name).toBe('bar.js');
         expect(tree.children[0].children[0].type).toBe('file');
         expect(tree.children[0].children[0].extension).toBe('.js');
 
-        expect(tree.children[0].children[1].path).toBe('__tests__/testdata/dir/main.js');
+        expect(tree.children[0].children[1].path).toBe(isWindows ? '__tests__\\testdata\\dir\\main.js' : '__tests__/testdata/dir/main.js');
         expect(tree.children[0].children[1].name).toBe('main.js');
         expect(tree.children[0].children[1].type).toBe('file');
         expect(tree.children[0].children[1].extension).toBe('.js');
 
-        expect(tree.children[0].children[2].path).toBe('__tests__/testdata/dir/main.ts');
+        expect(tree.children[0].children[2].path).toBe(isWindows ? '__tests__\\testdata\\dir\\main.ts' : '__tests__/testdata/dir/main.ts');
         expect(tree.children[0].children[2].name).toBe('main.ts');
         expect(tree.children[0].children[2].type).toBe('file');
         expect(tree.children[0].children[2].extension).toBe('.ts');
 
         //otherDir
-        expect(tree.children[1].path).toBe('__tests__/testdata/otherDir');
+        expect(tree.children[1].path).toBe(isWindows ? '__tests__\\testdata\\otherDir' : '__tests__/testdata/otherDir');
         expect(tree.children[1].name).toBe('otherDir');
         expect(tree.children[1].type).toBe('directory');
         expect(tree.children[1].children.length).toBe(2);
 
-        expect(tree.children[1].children[0].path).toBe('__tests__/testdata/otherDir/subDir');
+        expect(tree.children[1].children[0].path).toBe(isWindows ? '__tests__\\testdata\\otherDir\\subDir' : '__tests__/testdata/otherDir/subDir');
         expect(tree.children[1].children[0].name).toBe('subDir');
         expect(tree.children[1].children[0].type).toBe('directory');
         expect(tree.children[1].children[0].children.length).toBe(0);
 
-        expect(tree.children[1].children[1].path).toBe('__tests__/testdata/otherDir/test.json');
+        expect(tree.children[1].children[1].path).toBe(isWindows ? '__tests__\\testdata\\otherDir\\test.json' : '__tests__/testdata/otherDir/test.json');
         expect(tree.children[1].children[1].name).toBe('test.json');
         expect(tree.children[1].children[1].type).toBe('file');
         expect(tree.children[1].children[1].extension).toBe('.json');
